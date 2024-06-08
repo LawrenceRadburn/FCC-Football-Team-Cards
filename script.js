f1d9350c8ba17165a8639215a29eca9eb81b5bc7
Lawrence Radburn <lawrence@radnet.ca>
2024-06-08 18:03:40 -0400

    feat: add content

--- script.js
+++ script.js
@@ -187,6 +187,8 @@ headCoach.textContent = coachName;
 const setPlayerCards = (arr = players) => {
   playerCards.innerHTML += arr.map( ( { name, position, number, isCaptainn, nickname }) => {
     `<div class="player-card">
-    <h2>${name}</h2></div>`
+          <h2>${isCaptain ? "(Captain)" : ""}${name}</h2>
+          <p>Position: ${position}</p>
+        </div>`
   });
 };
