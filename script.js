6dfc2de6fe22a20909130fca3f87dd3e5bbbf6f7
Lawrence Radburn <lawrence@radnet.ca>
2024-06-11 14:36:12 -0400

    feat: dropdown switch case display

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const myFavoriteFootballTeam = {
     isWorldCupWinner: true,
     headCoach: {
         coachName: "Carlos Bilardo",
-        matches: 7
+        matches: 7,
     },
     players: [
         {
@@ -20,14 +20,14 @@ const myFavoriteFootballTeam = {
             position: "forward",
             number: 1,
             isCaptain: false,
-            nickname: null
+            nickname: null,
         },
         {
             name: "Sergio Batista",
             position: "midfielder",
             number: 2,
             isCaptain: false,
-            nickname: null
+            nickname: null,
         },
         {
             name: "Ricardo Bochini",
@@ -168,16 +168,13 @@ const myFavoriteFootballTeam = {
             number: 22,
             isCaptain: false,
             nickname: null,
-          }
+          },
     ],
 };
 
 Object.freeze(myFavoriteFootballTeam);
-
-
-
 const { sport, team, year, players } = myFavoriteFootballTeam;
-const { headCoach: { coachName } } = myFavoriteFootballTeam;
+const { coachName } = myFavoriteFootballTeam.headCoach;
 
 typeOfSport.textContent = sport;
 teamName.textContent = team;
@@ -185,16 +182,34 @@ worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
 const setPlayerCards = (arr = players) => {
-  playerCards.innerHTML += arr.map( ( { name, position, number, isCaptainn, nickname }) => {
+  playerCards.innerHTML += arr.map( ( { name, position, number, isCaptain, nickname }) => 
     `<div class="player-card">
           <h2>${isCaptain ? "(Captain)" : ""}${name}</h2>
           <p>Position: ${position}</p>
           <p>Number: ${number}</p>
-          <p>Nickname: ${nickname ? nickname : "N/A"}</p>
+          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
         </div>`
-  }).join("");
+  ).join("");
 };
 
 playersDropdownList.addEventListener("change", (e) => {
-  console.log(e.target.value);
+  playerCards.innerHTML = "";
+  switch (e.target.value) {
+    case "nickname":
+      setPlayerCards(players.filter((player) => player.nickname !== null));
+      break;
+    case "forward":
+      setPlayerCards(players.filter((player) => player.position === "forward"));
+      break;
+      case "midfielder":
+        setPlayerCards(players.filter((player) => player.position === "midfielder"));
+      break;
+      case "defender":
+        setPlayerCards(players.filter((player) => player.position === "defender"));
+        break;
+      case "goalkeeper":
+        setPlayerCards(players.filter((player) => player.position === "goalkeeper"));
+        break;
+        default: setPlayerCards();
+  }
 });
